3a19e007ec6e5555cbc88eb185d000831c9ce952
ozkrpy <ozkrpy@gmail.com>
2020-08-19 14:04:07 -0400

    funcionlidad para iniciar automaticamente el cliente FortiClient

--- constantes.py
+++ constantes.py
@@ -1,2 +1,3 @@
 URL = 'https://s.personal.com.py/teletrabajo/'
 LINEA = '595971325507'
+FIREFOX_PERFIL = ''
--- teletrabajo.py
+++ teletrabajo.py
@@ -3,68 +3,63 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 from time import sleep
 import constantes
 
 class Bot():
     def __init__(self):    
-        print("Cargando sitio web.")
         self.opts = Options()
         self.opts.headless = True
-        self.browser = Firefox()#options=opts)
-        self.browser.get(
-            constantes.URL # 'https://s.personal.com.py/teletrabajo/'
-        )
-        self.button_next = self.browser.find_element_by_id('nextBtn')
-        self.button_next.click()
-        self.linea = input('LINEA (59597XXXXXXX):')
-        self.numero_linea = self.browser.find_element_by_name('cphone')
-        if self.linea == '':
-            self.numero_linea.send_keys(constantes.LINEA)
+        if (constantes.FIREFOX_PERFIL == ''):
+            print('Es necesario agregar el path del perfil activo de Firefox.')
+            print('Editar el archivo: constantes.py.')
+            print('Ejemplo: FIREFOX_PERFIL=C:/Users/usuario1/AppData/Roaming/Mozilla/Firefox/Profiles/<XXXXXXXX>.default-release')
         else:
-            self.numero_linea.send_keys(self.linea)
-        self.button_next.click()
-        print("Aguarda unos segundos para recepcionar el PIN.")
-        sleep(3)
-        self.button_next.click()
-        self.pin = input('PIN:')
-        self.numero_pin = self.browser.find_element_by_name('pin')
-        self.numero_pin.send_keys(self.pin)
-        self.button_next.click()
-        try:
-            self.wait = WebDriverWait(self.browser, 10).until(
-                EC.presence_of_element_located((By.ID, "fo1"))
+            self.opts.profile = constantes.FIREFOX_PERFIL        
+            self.browser = Firefox(options=self.opts)
+            print("Cargando sitio web.")
+            self.browser.get(
+                constantes.URL
             )
-            self.activar_forti = self.browser.find_element_by_id("fo1")
+            self.button_next = self.browser.find_element_by_id('nextBtn')
+            self.button_next.click()
+            self.linea = input('LINEA (59597XXXXXXX):')
+            self.numero_linea = self.browser.find_element_by_name('cphone')
+            if self.linea == '':
+                self.numero_linea.send_keys(constantes.LINEA)
+            else:
+                self.numero_linea.send_keys(self.linea)
+            self.button_next.click()
+            print("Aguarda unos segundos para recepcionar el PIN.")
             sleep(3)
-            self.reconectarse = 's'
-            while self.reconectarse.lower()=='s':
-                self.activar_forti.click()
-                sleep(2)
-                self.obj = self.browser.switch_to.alert
-                self.msg=self.obj.text
-                print ("Estado: "+ self.msg)
-                sleep(1)
-                self.obj.accept()
-                print("Listo, ya se puede acceder al VPN.")
+            self.button_next.click()
+            self.pin = input('PIN:')
+            self.numero_pin = self.browser.find_element_by_name('pin')
+            self.numero_pin.send_keys(self.pin)
+            self.button_next.click()
+            try:
+                self.wait = WebDriverWait(self.browser, 10).until(
+                    EC.presence_of_element_located((By.ID, "fo1"))
+                )
+                self.activar_forti = self.browser.find_element_by_id("fo1")
+                sleep(3)
+                self.reconectarse = 's'
+                while self.reconectarse.lower()=='s':
+                    self.activar_forti.click()
+                    sleep(2)
+                    self.obj = self.browser.switch_to.alert
+                    self.msg=self.obj.text
+                    print ("Estado: "+ self.msg)
+                    sleep(1)
+                    self.obj.accept()
+                    print("Listo, ya se puede acceder al VPN.")
+                    self.reconectarse = input('Reconectarse(s/n)?: ')
+            except Exception as error:
+                print("No se activo la sesion VPN:","{}".format(error))
+            self.browser.close()
+            print('Ya se puede cerrar la aplicacion.')
 
-                # NEED TO TEST THIS BLOCK: pop-up que solicita seleccionar FortiClient.
-                # Find our target window
-                # handle = page.driver.find_window("My window title")
-
-                # # Close it
-                # page.driver.browser.switch_to.window(handle)
-                # page.driver.browser.close
-
-                # # Have the Selenium driver point to another window
-                # last_handle = page.driver.browser.window_handles.last
-                # page.driver.browser.switch_to.window(last_handle)
-
-                self.reconectarse = input('Reconectarse(s/n)?: ')
-        except Exception as error:
-            print("No se activo la sesion VPN:","{}".format(error))
-        self.browser.close()
-        print('Ya se puede cerrar la aplicacion.')
 try:
     init_bot = Bot()
 except Exception as error:
