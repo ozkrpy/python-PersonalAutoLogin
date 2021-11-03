1a5c2740d5e325048e7e2c75b25cac391ac9d2f5
Oz Ruffinelli <ozkrpy@gmail.com>
2021-11-03 15:55:30 -0300

    correccion del .bat

--- TeletrabajoPersonal.bat
+++ TeletrabajoPersonal.bat
@@ -2,7 +2,7 @@ echo off
 echo BATCH: Accediendo al directorio...
 cd C:\PrivateApps\Proyectos\python-PersonalAutoLogin\
 echo BATCH: Ejecutar script...
-python.exe .\teletrabajo.py
+python.exe .\teletrabajo_chrome.py
 @REM echo BATCH: Configurando VPN
 @REM python.exe .\load_vpn_data.py
 exit
